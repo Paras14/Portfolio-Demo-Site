381b61040659fb2bf26a0849552366b3b7187b02
Paras <parasv249@gmail.com>
2023-03-15 11:10:57 +0100

    Style and add content to intro page

--- src/component/intro/Intro.jsx
+++ src/component/intro/Intro.jsx
@@ -3,6 +3,22 @@ import './intro.scss'
 
 export default function Intro() {
   return (
-    <div className='intro' id='intro'></div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assests/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className='wrapper'>
+          <h2>Hi There, I'm</h2>
+          <h1>John Doe</h1>
+          <h3>Frontend Developer<span></span></h3>
+        </div>
+        <a href='#portfolio'>
+          <img src='assests/down.png' alt='' />
+        </a>
+      </div>
+    </div>
   )
 }
